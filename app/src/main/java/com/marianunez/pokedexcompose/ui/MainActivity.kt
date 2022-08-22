8de0ff9bdafa468783b0c13c1a5f7d071787655e
maria.nunez <maria.nunez@wefox.com>
2022-08-22 17:08:30 +0200

    navigation

--- app/src/main/java/com/marianunez/pokedexcompose/ui/MainActivity.kt
+++ app/src/main/java/com/marianunez/pokedexcompose/ui/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.marianunez.pokedexcompose.ui.theme.PokedexComposeTheme
 
@@ -19,6 +20,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexComposeTheme {
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "pokemonList") {
+                    composable("pokemonList"){
+                        // TODO: poner la screen
+                    }
+                    composable("pokemonDetail"){
+                        // TODO: poner la screen
+                    }
+                }
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
