827ff4fa3ade5c287dbeca8e0b0c3c2c724b39f2
maria.nunez <maria.nunez@wefox.com>
2022-08-23 12:08:15 +0200

    add classes to koin module

--- app/src/main/java/com/marianunez/pokedexcompose/di/PokedexModule.kt
+++ app/src/main/java/com/marianunez/pokedexcompose/di/PokedexModule.kt
@@ -1,12 +1,18 @@
 package com.marianunez.pokedexcompose.di
 
+import com.marianunez.pokedexcompose.data.datasource.cloud.PokemonListCloudDataSource
+import com.marianunez.pokedexcompose.data.repository.PokemonListRepository
+import com.marianunez.pokedexcompose.data.repository.PokemonListRepositoryImpl
+import com.marianunez.pokedexcompose.domain.PokemonListUseCase
+import com.marianunez.pokedexcompose.ui.PokemonListViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val pokedexModule = module {
 
-    // TODO: add classes
-    /*
-    single {  }
-    */
+    single { PokemonListCloudDataSource(get()) }
+    single<PokemonListRepository> { PokemonListRepositoryImpl(get()) }
+    single { PokemonListUseCase(get()) }
 
+    viewModel { PokemonListViewModel(get()) }
 }
