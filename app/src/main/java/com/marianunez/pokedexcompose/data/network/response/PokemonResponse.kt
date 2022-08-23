bd25c3649c0ba9870058dc37020b42fc9a6e8389
maria.nunez <maria.nunez@wefox.com>
2022-08-23 11:27:32 +0200

    remove attrs

--- app/src/main/java/com/marianunez/pokedexcompose/data/network/response/PokemonResponse.kt
+++ app/src/main/java/com/marianunez/pokedexcompose/data/network/response/PokemonResponse.kt
@@ -3,12 +3,10 @@ package com.marianunez.pokedexcompose.data.network.response
 data class PokemonList(
     val count: Int = 0,
     val next: String = "",
-    val previous: String = "",
     val results: List<PokemonResult> = emptyList()
 )
 
 data class PokemonResult(
-    val id: String,
     val name: String = "",
     val url: String = ""
 )
