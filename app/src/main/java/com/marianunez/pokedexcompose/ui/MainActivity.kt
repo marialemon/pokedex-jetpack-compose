0f9e2025b41d0dd719715309859eb8595fc93009
maria.nunez <maria.nunez@wefox.com>
2022-08-29 15:00:56 +0200

    add viewmodel to activity

--- app/src/main/java/com/marianunez/pokedexcompose/ui/MainActivity.kt
+++ app/src/main/java/com/marianunez/pokedexcompose/ui/MainActivity.kt
@@ -3,27 +3,32 @@ package com.marianunez.pokedexcompose.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.marianunez.pokedexcompose.ui.theme.PokedexComposeTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: PokemonListViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             PokedexComposeTheme {
                 val navController = rememberNavController()
+                val pokemonList by viewModel.pokemonListResult.observeAsState(emptyList())
+
                 NavHost(navController = navController, startDestination = "pokemonList") {
                     composable("pokemonList") {
-                        PokemonListView(navController = navController)
+                        PokemonListView(navController = navController, pokemonList = pokemonList)
                     }
                     composable(route = "pokemonDetail/{pokemonName}", arguments = listOf(
                         navArgument("pokemonName") {
