f898e9ab4f2706327d6374ec34d534ecea656413
maria.nunez <maria.nunez@wefox.com>
2022-08-29 14:58:30 +0200

    add const

--- app/src/main/java/com/marianunez/pokedexcompose/data/network/ApiRestService.kt
+++ app/src/main/java/com/marianunez/pokedexcompose/data/network/ApiRestService.kt
@@ -1,14 +1,14 @@
 package com.marianunez.pokedexcompose.data.network
 
-import com.marianunez.pokedexcompose.data.network.response.PokemonList
+import com.marianunez.pokedexcompose.data.network.response.PokemonResult
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiRestService {
 
-    @GET("pokemon")
+    @GET(ApiConstants.POKEMON_LIST)
     suspend fun getPokemonList(
         @Query("limit") limit: Int = 20,
         @Query("offset") offset: Int = 20,
-    ): PokemonList
+    ): List<PokemonResult>
 }
